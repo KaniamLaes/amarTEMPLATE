8e35174bc5afac008985b36e9b39b27a83bf81bf
KaniamLaes <114933249+KaniamLaes@users.noreply.github.com>
2023-10-09 01:26:48 +0530

    Update modop.cpp

--- templates/modop.cpp
+++ templates/modop.cpp
@@ -1,15 +1,15 @@
 namespace modop {
-	ll madd(ll a, ll b) {
+	int madd(int a, int b) {
 	  return (a + b) % mod;
 	}
-	ll msub(ll a, ll b) {
+	int msub(int a, int b) {
 	  return (((a - b) % mod) + mod) % mod;
 	}
-	ll mmul(ll a, ll b) {
+	int mmul(int a, int b) {
 	  return ((a % mod) * (b % mod)) % mod;
 	}
-	ll mpow(ll base, ll exp) {
-	  ll res = 1;
+	int mpow(int base, int exp) {
+	  int res = 1;
 	  while (exp) {
 		if (exp % 2 == 1){
 			res = (res * base) % mod;
@@ -19,35 +19,35 @@ namespace modop {
 	  }
 	  return res;
 	}
-	ll minv(ll base) {
+	int minv(int base) {
 	  return mpow(base, mod - 2);
 	}
-	ll mdiv(ll a, ll b) {
+	int mdiv(int a, int b) {
 	  return mmul(a, minv(b));
 	}
 	
-	const ll FACTORIAL_SIZE = 1.1e6;
-	ll fact[FACTORIAL_SIZE], ifact[FACTORIAL_SIZE];
+	const int FACTORIAL_SIZE = 1.1e6;
+	int fact[FACTORIAL_SIZE], ifact[FACTORIAL_SIZE];
 	bool __factorials_generated__ = 0;
-	void gen_factorial(ll n) {
+	void gen_factorial(int n) {
 		__factorials_generated__ = 1;
 		fact[0] = fact[1] = ifact[0] = ifact[1] = 1;
 		
-		for (ll i = 2; i <= n; i++) {
+		for (int i = 2; i <= n; i++) {
 			fact[i] = (i * fact[i - 1]) % mod;
 		}
 		ifact[n] = minv(fact[n]);
-		for (ll i = n - 1; i >= 2; i--) {
+		for (int i = n - 1; i >= 2; i--) {
 			ifact[i] = ((i + 1) * ifact[i + 1]) % mod;
 		}
 	}
-	ll nck(ll n, ll k) {
+	int nck(int n, int k) {
 		if (!__factorials_generated__) {
 			cerr << "Call gen_factorial you dope" << endl;
 			exit(1);
 		}
 		if (k < 0 || n < k) return 0;
-		ll den = (ifact[k] * ifact[n - k]) % mod;
+		int den = (ifact[k] * ifact[n - k]) % mod;
 		return (den * fact[n]) % mod;
 	}
 }
